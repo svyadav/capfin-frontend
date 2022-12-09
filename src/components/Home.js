8c5d5501f27005953d769a4f71a4c5c942841269
sachin <ysachin511@gmail.com>
2022-12-09 13:42:34 +0530

    error msg

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,7 +32,7 @@ const Home=()=>{
               
             })
             .catch((err) => {
-              console.log(err);
+              console.log("This is errr",err);
             });
         };
         getUser(); 

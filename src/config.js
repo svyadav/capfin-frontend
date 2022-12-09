82102043362275a506ca442f5301bc0ad5d95645
sachin <ysachin511@gmail.com>
2022-12-09 13:57:11 +0530

    Chnage

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 
-const serverUrl = 'http://localhost:3000'
-const clientUrl = 'http://localhost:5000'
+const serverUrl = 'http://localhost:5000'
+const clientUrl = 'http://localhost:3000'
 module.exports = {
     serverUrl,
     clientUrl
